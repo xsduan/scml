4d6f0dd185bdf254513714f935d2949363a043ef
Shane Duan <s_duan@u.pacific.edu>
2017-12-11 14:54:37 -0800

    Add basic arithmetic for Point
    Implementation is currently incomplete, only adding for what is immediately required. Future updates should function like Vector2 in Unity.
    
    Reimplement StrokeDescription functions using operators.

--- src/process.rs
+++ src/process.rs
@@ -1,4 +1,5 @@
 use std::f32;
+use std::ops::*;
 use std::str;
 
 use parse::*;
@@ -7,13 +8,85 @@ impl Clone for Point {
     fn clone(&self) -> Point { *self }
 }
 
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl AddAssign for Point {
+    fn add_assign(&mut self, other: Point) {
+        *self = Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Point {
+    type Output = Point;
+    fn sub(self, other: Point) -> Point {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl SubAssign for Point {
+    fn sub_assign(&mut self, other: Point) {
+        *self = Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl Div<f32> for Point {
+    type Output = Point;
+    fn div(self, other: f32) -> Point {
+        Point {
+            x: self.x / other,
+            y: self.y / other,
+        }
+    }
+}
+
+impl DivAssign<f32> for Point {
+    fn div_assign(&mut self, other: f32) {
+        *self = Point {
+            x: self.x / other,
+            y: self.y / other,
+        }
+    }
+}
+
+impl Point {
+    fn min(self, other: Point) -> Point {
+        Point {
+            x: self.x.min(other.x),
+            y: self.y.min(other.y),
+        }
+    }
+
+    fn max(self, other: Point) -> Point {
+        Point {
+            x: self.x.max(other.x),
+            y: self.y.max(other.y),
+        }
+    }
+}
+
 impl StrokeDescription {
     fn top_left(&self) -> Point {
         let mut min_point = Point { x: f32::INFINITY, y: f32::INFINITY };
 
         for anchor in self.anchors.iter() {
-            min_point.x = min_point.x.min(anchor.point.x);
-            min_point.y = min_point.y.min(anchor.point.y);
+            min_point = min_point.min(anchor.point);
         }
 
         min_point
@@ -23,8 +96,7 @@ impl StrokeDescription {
         let mut max_point = Point { x: -f32::INFINITY, y: -f32::INFINITY };
 
         for anchor in self.anchors.iter() {
-            max_point.x = max_point.x.max(anchor.point.x);
-            max_point.y = max_point.y.max(anchor.point.y);
+            max_point = max_point.max(anchor.point);
         }
 
         max_point
@@ -35,13 +107,11 @@ impl StrokeDescription {
         let min_point = self.top_left();
 
         for i in 0..self.anchors.len() {
-            self.anchors[i].point.x -= min_point.x;
-            self.anchors[i].point.y -= min_point.y;
+            self.anchors[i].point -= min_point;
         }
 
         for i in 0..self.locations.len() {
-            self.locations[i].direction.x -= min_point.x;
-            self.locations[i].direction.y -= min_point.y;
+            self.locations[i].direction -= min_point;
         }
     }
 
@@ -52,13 +122,11 @@ impl StrokeDescription {
         let max_dimension = max_point.x.max(max_point.y);
 
         for i in 0..self.anchors.len() {
-            self.anchors[i].point.x /= max_dimension;
-            self.anchors[i].point.y /= max_dimension;
+            self.anchors[i].point /= max_dimension;
         }
 
         for i in 0..self.locations.len() {
-            self.locations[i].direction.x /= max_dimension;
-            self.locations[i].direction.y /= max_dimension;
+            self.locations[i].direction /= max_dimension;
         }
     }
 
